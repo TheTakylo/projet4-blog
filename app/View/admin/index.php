a2a2e03bf4d8549419fdfb2255b68665f004587b
TheTakylo <sebastien.thuret@yahoo.fr>
2019-05-04 20:06:32 +0200

    Admin index

--- app/View/admin/index.php
+++ app/View/admin/index.php
@@ -1,1 +1,13 @@
-Admin
+<?php $title = 'Accueil'; ?>
+
+<div class="row">
+   <div class="col-md-3">
+      <div class="card bg-light mb-3" style="max-width: 18rem;">
+         <div class="card-header">Chapitres</div>
+         <div class="card-body">
+            <a href="<?= Urls::route('admin@chapters'); ?>" class="btn btn-primary">Voir tous les chapitres</a>
+            <a href="#" class="btn btn-secondary mt-3">Écrire un chapitre</a>
+         </div>
+      </div>
+   </div>
+</div>
--- app/View/layouts/admin_layout.php
+++ app/View/layouts/admin_layout.php
@@ -59,6 +59,7 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Administration</h1>
       </div>
+      <?= $content ?>
     </main>
   </div>
 </div>
